28086a6508a66b64032572cd766894daa0b0edda
Tati650 <tatiponce06@gmail.com>
2025-06-12 09:58:23 -0300

    Update Scheme.sql

--- database/Scheme.sql
+++ database/Scheme.sql
@@ -16,9 +16,9 @@ CREATE DATABASE "Portal turistico"
 COMMENT ON DATABASE "Portal turistico"
     IS 'Portal de ventas de paquetes turisticos.';
 
-
+GO
 Create table Clientes (
-Cliente_ID PRIMARY KEY,
+Cliente_ID PRIMARY KEY IDENTITY (1,1) NOT NULL,
 Nombre VARCHAR(100),
 Apellidos VARCHAR(150),
 Nombre_Usuario VARCHAR (100),
@@ -30,8 +30,9 @@ Telefono INT (15),
 Email VARCHAR (150),
 )
 
+GO
 Create table Administradores(
-Admin_ID PRIMARY KEY,
+Admin_ID PRIMARY KEY IDENTITY (1,1) NOT NULL,
 Nombre VARCHAR(100),
 Apellidos VARCHAR (150),
 Nombre_Usuario VARCHAR (100),
@@ -42,7 +43,64 @@ Direccion VARCHAR (150),
 Telefono INT (15),
 Email VARCHAR (150),
 )
+GO
+Create table Paquete(
+Paquete_ID PRIMARY KEY IDENTITY (1,1) NOT NULL,
+Destino VARCHAR (3),//nac int
+CantViajeros int (2),
+Precio MONEY ()
+)
 
-Create table Catalogo(
+GO
+Create table ProductosPaquete(
+Paquete_ID
+viaje_ID
+AlojamientoID
+)
 
+GO
+Create table Producto(
+Producto_ID PRIMARY KEY IDENTITY (1,1) NOT NULL,
+Tipo_Producto VARCHAR (255),
+
+)
+GO
+Create table viaje(
+Viaje_ID PRIMARY KEY IDENTITY (1,1) NOT NULL,
+Tipo_Viaje VARCHAR (255),
+Destino VARCHAR (255),
+FechaIda DATETIME,
+FechaVuelta DATETIME,
+Precio MONEY,
+)
+GO
+Create table Alojamiento(
+Alojamiento_ID PRIMARY KEY IDENTITY (1,1) NOT NULL,
+Hotel VARCHAR (100),
+Servicios VARCHAR (255),
+Desayuno VARCHAR (255),
+Fecha_Inicio DATETIME ()
+Fecha_Final DATETIME ()
+Ubicacion VARCHAR (255)
+Precio MONEY,
+)
+
+GO
+Create table Carrito(
+Compra_ID
+Producto_ID
+Paquete_ID,
+Vencimiento DATE,
+)
+
+GO
+Create table DetallesCompra(
+Compra_ID PRIMARY KEY IDENTITY (1,1) NOT NULL,
+Producto_ID
+Paquete_ID
+Tipo_Paquete
+Cantidad_Paquete
+Cantidad_Producto
+Direccion
+Pais
 )
